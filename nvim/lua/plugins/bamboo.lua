cc4ea22a0c5f5e7852d988fb0bd75746a18f1d72
IsraelBoka <israelboka6@gmail.com>
2025-08-21 17:46:23 +0000

    feat: add neovim to it

---
+++ nvim/lua/plugins/bamboo.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup({
+        transparent = true,
+        lualine = {
+          transparent = true,
+        },
+      })
+    end,
+  },
+}
